8686bc1b2f82a0aea57befefded33cd55559bd6e
TunyakarnK <tunyakarn2544@gmail.com>
2021-09-09 15:09:34 +0700

    3

--- src/components/Repiles.tsx
+++ src/components/Repiles.tsx
@@ -11,6 +11,10 @@ const Repiles = ({username,userImagePath,commentText,likeNum,replies}:CommentTyp
             <div className="bg-gray-500 rounded-lg p-2">
               <p className="font-semibold text-white">{username}</p>
               <p className='text-white'>{commentText}</p>
+              <div className='flex items-center'>
+                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                <p className='text-gray-300'>{likeNum}</p>
+              </div>
             </div>
           </div>
           </div>
